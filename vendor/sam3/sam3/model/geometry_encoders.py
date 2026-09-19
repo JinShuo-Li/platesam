29abab39150dac24ea07fc9b283eb47a7359eaed
kkl <kingtu1040@sjtu.edu.cn>
2026-09-20 00:18:24 +0800

    一处硬件检测条件修复

--- vendor/sam3/sam3/model/geometry_encoders.py
+++ vendor/sam3/sam3/model/geometry_encoders.py
@@ -648,7 +648,10 @@ class SequenceGeometryEncoder(nn.Module):
             # We need to denormalize, and convert to [x, y, x, y]
             boxes_xyxy = box_cxcywh_to_xyxy(boxes)
             scale = torch.tensor([W, H, W, H], dtype=boxes_xyxy.dtype)
-            scale = scale.pin_memory().to(device=boxes_xyxy.device, non_blocking=True)
+            #scale = scale.pin_memory().to(device=boxes_xyxy.device, non_blocking=True)
+            if torch.accelerator.is_available():  # 仅在存在加速器（CUDA/XPU）时锁定内存
+                scale = scale.pin_memory()
+            scale = scale.to(device=boxes_xyxy.device, non_blocking=True)
             scale = scale.view(1, 1, 4)
             boxes_xyxy = boxes_xyxy * scale
             sampled = torchvision.ops.roi_align(
